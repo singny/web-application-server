b7597a30e1ea2505bd074afeea997e927458491f
sieun park <89372116+singny@users.noreply.github.com>
2024-08-20 17:37:29 +0900

    Add files via upload

--- src/main/java/full/HttpServer.java
+++ src/main/java/full/HttpServer.java
@@ -1,10 +1,8 @@
 package full;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -42,27 +40,9 @@ public class HttpServer {
     }
 
     public static void main(String[] args) {
-//        Map<String, File> virtualHosts = new HashMap<>();
-        
-        // 호스트별 루트 디렉터리를 설정합니다.
-//        virtualHosts.put("a.com", new File("src/main/resources/a_com_root"));
-//        virtualHosts.put("b.com", new File("src/main/resources/b_com_root"));
-//        virtualHosts.put("default", new File("src/main/resources/a_com_root"));
-        
-        // set the port to listen on
-//        int port;
-//        int port = 9090;
-//        try {
-//            port = Integer.parseInt(args[0]);
-//            if (port < 0 || port > 65535) port = 80;
-//        } catch (RuntimeException ex) {
-//            port = 80;
-//        }
-     
         try {
             ServerConfig config = new ServerConfig("src/main/resources/config.json");
             HttpServer webserver = new HttpServer(config);
-//            HttpServer webserver = new HttpServer(virtualHosts, port);
             webserver.start();
         } catch (IOException ex) {
             logger.log(Level.SEVERE, "Server could not start", ex);
